8d7c3825fc167b1d697b7de92d0cb71953f64b47
Kay Eugenia Purnama <122983473+kayeugenia@users.noreply.github.com>
2024-02-03 22:30:56 +0800

    append relevant course to result dictionary

--- app/data.js
+++ app/data.js
@@ -1,36 +1,42 @@
 export const quiz = {
-    totalQuestions: 5,
+    totalQuestions: 2,
     questions: [
       {
         id: 1,
-        question: 'What is the capital of France?',
-        answers: ['Madrid', 'Paris', 'Rome', 'Berlin'],
-        correctAnswer: 'Paris',
+        question: 'What are your fields of interest?',
+        answers: ['Urban Planning', 'Technology', 'Strategy', 'Anthropology', 'Healthcare', 'Environment'],
+        mappedAnswer: {
+            'Urban Planning': ['UTS2105/UTC2105', 'UTC2109/UTS2109'],
+            'Technology': ['UTS2114/UTC2114', 'UTS2103/UTC2103', 'UTS2100/UTC2100'],
+            'Strategy': ['UTC2113', 'UTS2100/UTC2100'],
+            'Anthropology': ['UTS2110/UTC2110', 'UTC2111'],
+            'Healthcare': ['UTS2103/UTC2103'],
+            'Environment': ['UTC2111', 'UTC2102']
+        },
       },
       {
         id: 2,
-        question: 'What is the largest planet in our solar system?',
-        answers: ['Mars', 'Jupiter', 'Venus', 'Saturn'],
-        correctAnswer: 'Jupiter',
+        question: 'What do you do in your free time?',
+        answers: ['Playing board games', 'Spending time with family', 'Volunteering', 'Crafting'],
+        mappedAnswer: {
+            'Playing board games' : ['UTC2113', 'UTS2100/UTC2100'],
+            'Spending time with family' : ['UTS2114/UTC2114', 'UTS2110/UTC2110'],
+            'Volunteering' : ['UTS2114/UTC2114'],
+            'Crafting' : ['UTS2105/UTC2105'],
+        }
       },
       {
         id: 3,
-        question: 'What is the smallest country in the world?',
-        answers: ['Monaco', 'Maldives', 'Vatican City', 'San Marino'],
-        correctAnswer: 'Vatican City',
-      },
-      {
-        id: 4,
-        question: 'What is the most widely spoken language in the world?',
-        answers: ['English', 'Mandarin', 'Spanish', 'Hindi'],
-        correctAnswer: 'Mandarin',
-      },
-      {
-        id: 5,
-        question: 'Who is the founder of Microsoft?',
-        answers: ['Steve Jobs', 'Bill Gates', 'Elon Musk', 'Mark Zuckerberg'],
-        correctAnswer: 'Bill Gates',
+        question: 'What is your favourite TV Show?',
+        answers: ['The Good Doctor', 'Black Mirror', 'Back to the Future', 'How to Get Away with Murder', 'The Good Place', 'Our Planet'],
+        mappedAnswer: {
+            'The Good Doctor' : ['UTS2103/UTC2103'],
+            'Black Mirror' : ['UTS2100/UTC2100', 'UTC2113'],
+            'Back to the Future' : ['UTC2101'],
+            'How to Get Away with Murder' : ['UTC2118'],
+            'The Good Place' : ['UTS2110/UTC2110', 'UTC2111'], 
+            'Our Planet' : ['UTC2102'], 
+        }
       },
     ],
   };
-  
--- app/quiz/page.jsx
+++ app/quiz/page.jsx
@@ -3,114 +3,101 @@ import React, { useState } from 'react';
 import { quiz } from '../data.js';
 
 const page = () => {
-  const [activeQuestion, setActiveQuestion] = useState(0);
-  const [selectedAnswer, setSelectedAnswer] = useState('');
-  const [checked, setChecked] = useState(false);
-  const [selectedAnswerIndex, setSelectedAnswerIndex] = useState(null);
-  const [showResult, setShowResult] = useState(false);
-  const [result, setResult] = useState({
-    score: 0,
-    correctAnswers: 0,
-    wrongAnswers: 0,
-  });
+    //increment quiz question
+    const [activeQuestion, setActiveQuestion] = useState(0);
+    //true if answer is selected
+    const [checked, setChecked] = useState(false);
+    //currently selected answer index
+    const [selectedAnswerIndex, setSelectedAnswerIndex] = useState(null);
+    //currently selected answer
+    const [selectedAnswer, setSelectedAnswer] = useState('');
+    //check if result page should be shown
+    const [showResult, setShowResult] = useState(false);
+    //output for result page
+    const [result, setResult] = useState({});
 
-  const { questions } = quiz;
-  const { question, answers, correctAnswer } = questions[activeQuestion];
+    const { questions } = quiz;
+    const { question, answers, mappedAnswer } = questions[activeQuestion];
 
-  //   Select and check answer
-  const onAnswerSelected = (answer, idx) => {
-    setChecked(true);
-    setSelectedAnswerIndex(idx);
-    if (answer === correctAnswer) {
-      setSelectedAnswer(true);
-      console.log('true');
-    } else {
-      setSelectedAnswer(false);
-      console.log('false');
-    }
-  };
+    //   Select answer
+    const onAnswerSelected = (answer, idx) => {
+        setChecked(true);
+        setSelectedAnswerIndex(idx);
+        setSelectedAnswer(answer);
+    };
 
-  // Calculate score and increment to next question
-  const nextQuestion = () => {
-    setSelectedAnswerIndex(null);
-    setResult((prev) =>
-      selectedAnswer
-        ? {
-            ...prev,
-            score: prev.score + 5,
-            correctAnswers: prev.correctAnswers + 1,
-          }
-        : {
-            ...prev,
-            wrongAnswers: prev.wrongAnswers + 1,
-          }
+    // Add the relevant courses to the result state
+    const nextQuestion = () => {
+        setSelectedAnswerIndex(null);
+        const coursesArray = mappedAnswer[selectedAnswer];
+        
+        coursesArray.forEach((course) => {
+            setResult((prev) => ({
+                ...prev,
+                [course]: prev[course] ? prev[course] + 1 : 1,
+            }));
+        });
+        
+        if (activeQuestion !== questions.length - 1) {
+        setActiveQuestion((prev) => prev + 1);
+        } else {
+        setActiveQuestion(0);
+        setShowResult(true);
+        }
+        setChecked(false);
+    };
+
+    return (
+        <div className='container-container'>
+            <div className='container'>
+            <h1>Quiz Page</h1>
+            <div>
+                {!showResult ? <h2>
+                Question: {activeQuestion + 1}
+                <span>/{questions.length}</span>
+                </h2> : null}
+            </div>
+            <div>
+                {!showResult ? (
+                <div className='quiz-container'>
+                    <h3>{questions[activeQuestion].question}</h3>
+                    {answers.map((answer, idx) => (
+                    <li
+                        key={idx}
+                        onClick={() => onAnswerSelected(answer, idx)}
+                        className={
+                        selectedAnswerIndex === idx ? 'li-selected' : 'li-hover'
+                        }
+                    >
+                        <span>{answer}</span>
+                    </li>
+                    ))}
+                    {checked ? (
+                    <button onClick={nextQuestion} className='btn'>
+                        {activeQuestion === question.length - 1 ? 'Finish' : 'Next'}
+                    </button>
+                    ) : (
+                    <button onClick={nextQuestion} disabled className='btn-disabled'>
+                        {' '}
+                        {activeQuestion === question.length - 1 ? 'Finish' : 'Next'}
+                    </button>
+                    )}
+                </div>
+                ) : (
+                <div className='quiz-container'>
+                    <h3>Results</h3>
+                    {Object.entries(result).map(([course, count]) => (
+                        <div key={course}>
+                            <div>{course}</div>
+                            <div>{count}</div>
+                        </div>
+                    ))}
+                </div>
+                )}
+            </div>
+            </div>
+        </div>
     );
-    if (activeQuestion !== questions.length - 1) {
-      setActiveQuestion((prev) => prev + 1);
-    } else {
-      setActiveQuestion(0);
-      setShowResult(true);
-    }
-    setChecked(false);
-  };
-
-  return (
-    <div className='container'>
-      <h1>Quiz Page</h1>
-      <div>
-        <h2>
-          Question: {activeQuestion + 1}
-          <span>/{questions.length}</span>
-        </h2>
-      </div>
-      <div>
-        {!showResult ? (
-          <div className='quiz-container'>
-            <h3>{questions[activeQuestion].question}</h3>
-            {answers.map((answer, idx) => (
-              <li
-                key={idx}
-                onClick={() => onAnswerSelected(answer, idx)}
-                className={
-                  selectedAnswerIndex === idx ? 'li-selected' : 'li-hover'
-                }
-              >
-                <span>{answer}</span>
-              </li>
-            ))}
-            {checked ? (
-              <button onClick={nextQuestion} className='btn'>
-                {activeQuestion === question.length - 1 ? 'Finish' : 'Next'}
-              </button>
-            ) : (
-              <button onClick={nextQuestion} disabled className='btn-disabled'>
-                {' '}
-                {activeQuestion === question.length - 1 ? 'Finish' : 'Next'}
-              </button>
-            )}
-          </div>
-        ) : (
-          <div className='quiz-container'>
-            <h3>Results</h3>
-            <h3>Overall {(result.score / 25) * 100}%</h3>
-            <p>
-              Total Questions: <span>{questions.length}</span>
-            </p>
-            <p>
-              Total Score: <span>{result.score}</span>
-            </p>
-            <p>
-              Correct Answers: <span>{result.correctAnswers}</span>
-            </p>
-            <p>
-              Wrong Answers: <span>{result.wrongAnswers}</span>
-            </p>
-            <button onClick={() => window.location.reload()}>Restart</button>
-          </div>
-        )}
-      </div>
-    </div>
-  );
 };
 
 export default page;
