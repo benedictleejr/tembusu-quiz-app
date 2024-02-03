928053e5879bfb29df5cf889265a66b8d5d7fe79
Kay Eugenia Purnama <122983473+kayeugenia@users.noreply.github.com>
2024-02-03 20:17:11 +0800

    style frontend

--- app/page.js
+++ app/page.js
@@ -4,7 +4,8 @@ export default function Home() {
   return (
     <main>
       <div className='container'>
-        <h1>Quiz App</h1>
+        <img src='/tembulogo.png' alt='Tembusu Logo' height='110vh'/>
+        <h1>Find the perfect courses for you!</h1>
         <Link href='/quiz'>
           <button>Start Quiz</button>
         </Link>
