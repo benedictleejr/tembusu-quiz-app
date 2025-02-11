a8723c497acb78c75db952a938615ba30f40d3a9
benedictleejr <belasolee@gmail.com>
2025-02-11 16:48:30 +0800

    Update courses for OH 2025

--- app/courseDataToCsv.py
+++ app/courseDataToCsv.py
@@ -2,54 +2,14 @@ import csv
 
 # Entire courseData dictionary
 courseData = {
-    'UTC2112/UTS2112': {
-        'name': 'Animals and the City',
-        'prof': '',
-        'type': 'Senior Seminar',
-    },
-    'UTC2118': {
-        'name': 'The Anatomy of Crime',
-        'prof': 'Dr Serena Seah',
-        'type': 'Senior Seminar',
-    },
-    'UTC2109/UTS2109': {
-        'name': 'Asia Now! The Archaeology of the Future City',
-        'prof': 'Dr Margaret Tan',
-        'type': 'Senior Seminar',
-    },
-    'UTS2103/UTC2103': {
-        'name': 'Biomedicine and Singapore Society',
-        'prof': 'Dr Serena Seah',
-        'type': 'Senior Seminar',
-    },
-    'UTC2102': {
-        'name': 'Climate Change',
-        'prof': 'Dr Connor Graham',
-        'type': 'Senior Seminar',
-    },
-    'UTC2113': {
-        'name': 'Gaming Life',
-        'prof': 'Ms Cera Tan',
-        'type': 'Senior Seminar',
-    },
     'UTS2110/UTC2110': {
         'name': 'Happiness By Design',
-        'prof': 'Dr John Wee',
-        'type': 'Senior Seminar',
-    },
-    'UTS2100/UTC2100': {
-        'name': 'Intelligence and Singapore Society',
-        'prof': 'Dr Connor Graham, Mr Shamraz Anver',
-        'type': 'Senior Seminar',
-    },
-    'UTC2108': {
-        'name': 'Knowledge and Expertise',
-        'prof': 'Dr Connor Graham, Dr Eric Kerr',
+        'prof': 'Dr John Wee, Dr Priscilla Koh',
         'type': 'Senior Seminar',
     },
     'UTC2107': {
         'name': 'Negotiating in a Complex World',
-        'prof': 'Dr Michael Grainger, Dr Kuan Yee Han',
+        'prof': 'Dr Kuan Yee Han, Dr Michael Grainger',
         'type': 'Senior Seminar',
     },
     'UTC2111': {
@@ -57,29 +17,19 @@ courseData = {
         'prof': 'Mr Shamraz Anver',
         'type': 'Senior Seminar',
     },
-    'UTS2105/UTC2105': {
+    'UTS2105': {
         'name': 'Singapore as "Model" City?',
-        'prof': 'Dr Margaret Tan',
+        'prof': 'Dr Connor Graham, Dr Margaret Tan',
         'type': 'Senior Seminar',
     },
     'UTC2117': {
         'name': 'Skin',
-        'prof': 'Dr Celine Coderey',
+        'prof': 'Dr Céline Coderey',
         'type': 'Senior Seminar',
     },
-    'UTS2114/UTC2114': {
-        'name': 'Technologies and Ageing in Singapore',
-        'prof': 'Dr Kuan Yee Han',
-        'type': 'Senior Seminar',
-    },
-    'UTC2116/UTS2116': {
+    'UTS2116': {
         'name': 'The University Today',
-        'prof': 'Dr Gene Navera',
-        'type': 'Senior Seminar',
-    },
-    'UTC2101': {
-        'name': 'Time and Life',
-        'prof': 'Dr Céline Coderey, Dr Eric Kerr',
+        'prof': 'Dr Connor Graham',
         'type': 'Senior Seminar',
     },
     'UTC1119': {
@@ -97,50 +47,30 @@ courseData = {
         'prof': 'Dr John van Wyhe',
         'type': 'Junior Seminar',
     },
-    'UTC1120': {
-        'name': 'Emotions and Society',
-        'prof': 'Mr Shamraz Anver',
-        'type': 'Junior Seminar',
-    },
     'UTC1102C': {
         'name': 'Fakes',
         'prof': 'Dr Eric Kerr',
         'type': 'Junior Seminar',
     },
     'UTC1113': {
-        'name': 'Ignorance and Uncertainty',
+        'name': 'Ignorance and Knowing',
         'prof': 'Dr John Wee',
         'type': 'Junior Seminar',
     },
-    'UTC1102D': {
-        'name': 'Images',
-        'prof': 'Associate Professor John Phillips',
-        'type': 'Junior Seminar',
-    },
-    'UTC1102S': {
-        'name': 'Living and Dying in the Internet Age',
-        'prof': 'Dr Connor Graham',
-        'type': 'Junior Seminar',
-    },
-    'UTC1102P': {
-        'name': 'Murals: Expressions from/on the Walls',
-        'prof': 'Dr Margaret Tan',
-        'type': 'Junior Seminar',
-    },
     'UTC1102G': {
-        'name': "Proof: What's truth got to do with it?",
+        'name': "Proof: What's Truth got to do with it?",
         'prof': 'Professor Tay Yong Chiang',
         'type': 'Junior Seminar',
     },
-    'UTC1102E': {
-        'name': 'Social Innovation',
-        'prof': 'Dr Dilum Wewalaarachchi',
-        'type': 'Junior Seminar',
+    'UTS2103': {
+        'name': "Health and the Community in Singapore",
+        'prof': "Dr Rafi Rashid",
+        'type': "Senior Seminar"
     },
-    'UTC1123': {
-        'name': 'The Nuclear Imagination',
-        'prof': 'Dr Rafi Rashid',
-        'type': 'Junior Seminar',
+    'UTS2104': {
+        'name': 'Intelligence and Singapore Society',
+        'prof': 'Mr Shamraz Anver, Ms Samridhi Aggarwal',
+        'type': 'Senior Seminar',
     }
 }
 
--- app/quizData.js
+++ app/quizData.js
@@ -6,12 +6,12 @@ export const quiz = {
         question: 'What is your field of interest?',
         answers: ['Urban Planning', 'Technology', 'Strategy', 'Anthropology', 'Healthcare', 'Environment'],
         mappedAnswer: {
-            'Urban Planning': ['UTS2105/UTC2105', 'UTC2109/UTS2109'],
-            'Technology': ['UTS2114/UTC2114', 'UTS2103/UTC2103'],
-            'Strategy': ['UTC2113', 'UTS2100/UTC2100'],
+            'Urban Planning': ['UTS2105', 'UTC1115'],
+            'Technology': ['UTS2104', 'UTS2103'],
+            'Strategy': ['UTC2111', 'UTS2104'],
             'Anthropology': ['UTS2110/UTC2110', 'UTC2117'],
-            'Healthcare': ['UTS2103/UTC2103'],
-            'Environment': ['UTC2111', 'UTC2102']
+            'Healthcare': ['UTS2103'],
+            'Environment': ['UTC2111', 'UTC1102B']
         },
       },
       {
@@ -19,10 +19,10 @@ export const quiz = {
         question: 'What do you do in your free time?',
         answers: ['Playing board games', 'Spending time with family', 'Volunteering', 'Crafting'],
         mappedAnswer: {
-            'Playing board games' : ['UTC2113', 'UTS2100/UTC2100'],
-            'Spending time with family' : ['UTS2114/UTC2114', 'UTS2110/UTC2110'],
-            'Volunteering' : ['UTC2111','UTC2116/UTS2116'],
-            'Crafting' : ['UTS2105/UTC2105'],
+            'Playing board games' : ['UTC1102G', 'UTS2104'],
+            'Spending time with family' : ['UTC2107', 'UTS2110/UTC2110'],
+            'Volunteering' : ['UTC2111','UTS2116'],
+            'Crafting' : ['UTS2105'],
         }
       },
       {
@@ -30,12 +30,12 @@ export const quiz = {
         question: 'What is your favourite TV Show?',
         answers: ['The Good Doctor', 'Black Mirror', 'Back to the Future', 'How to Get Away with Murder', 'The Good Place', 'Our Planet'],
         mappedAnswer: {
-            'The Good Doctor' : ['UTS2103/UTC2103'],
-            'Black Mirror' : ['UTS2100/UTC2100', 'UTC2113'],
-            'Back to the Future' : ['UTC2101'],
-            'How to Get Away with Murder' : ['UTC2118'],
+            'The Good Doctor' : ['UTS2103'],
+            'Black Mirror' : ['UTS2104', 'UTC1113'],
+            'Back to the Future' : ['UTC1113', 'UTC1102G'],
+            'How to Get Away with Murder' : ['UTC1119'],
             'The Good Place' : ['UTS2110/UTC2110', 'UTC2111'], 
-            'Our Planet' : ['UTC2102'], 
+            'Our Planet' : ['UTC1102B'], 
         }
       },
       {
@@ -43,12 +43,12 @@ export const quiz = {
         question: "Which book genre do you enjoy reading?",
         answers: ['Science fiction', 'Mystery', 'Historical', 'Non-fiction', 'Fantasy', 'Dystopian'],
         mappedAnswer: {
-            'Science fiction' : ['UTS2103/UTC2103','UTC2108','UTC1115'],
-            'Mystery' : ['UTC2118','UTC1119'],
-            'Historical' : ['UTC2101','UTC1102B'],
-            'Non-fiction' : ['UTS2100/UTC2100','UTC2107','UTC1113'],
-            'Fantasy' : ['UTS2105/UTC2105','UTC1102C','UTC1102P'], 
-            'Dystopian' : ['UTS2114/UTC2114', 'UTC1102S'], 
+            'Science fiction' : ['UTC1115','UTC1102G','UTC1113'],
+            'Mystery' : ['UTC1119'],
+            'Historical' : ['UTC1102B'],
+            'Non-fiction' : ['UTS2104','UTC2107','UTC1113'],
+            'Fantasy' : ['UTS2105','UTC1102C'], 
+            'Dystopian' : ['UTC1102C'], 
         }
       },
       {
@@ -56,11 +56,11 @@ export const quiz = {
         question: "What social issue concerns you the most?",
         answers: ['Poverty alleviation', 'Education disparities', 'Racial justice', 'Environmental sustainability', 'War and conflict'],
         mappedAnswer: {
-            'Poverty alleviation' : ['UTC2111','UTC1102E','UTC1120'],
-            'Education disparities' : ['UTC2116/UTS2116','UTC1102G'],
-            'Racial justice' : ['UTC2117','UTC1102D'],
-            'Environmental sustainability' : ['UTC2102'],
-            'War and conflict' : ['UTC2107', 'UTC2118', 'UTC1123'],
+            'Poverty alleviation' : ['UTC2111','UTS2103'],
+            'Education disparities' : ['UTS2116','UTC1102G'],
+            'Racial justice' : ['UTC2117','UTC1102C'],
+            'Environmental sustainability' : ['UTC2111', 'UTS2105'],
+            'War and conflict' : ['UTC2107', 'UTC1119'],
         }
       },
       {
@@ -68,12 +68,12 @@ export const quiz = {
         question: "What is your preferred learning approach?",
         answers: ['Research essay', 'Creative works/Exhibition', 'Project that engages real stakeholders','Debate','Individual reflection','Field trip'],
         mappedAnswer: {
-            'Research essay' : ['UTC2108'],
-            'Creative works/Exhibition': ['UTC2101','UTC2117'],
-            'Project that engages real stakeholders' : ['UTS2105/UTC2105'],
+            'Research essay' : ['UTC1113', 'UTC1102G'],
+            'Creative works/Exhibition': ['UTS2116','UTC2117'],
+            'Project that engages real stakeholders' : ['UTS2105'],
             'Debate' : ['UTC2107'],
-            'Individual reflection' : ['UTC2101','UTC2116/UTS2116'],
-            'Field trip': ['UTC2109/UTS2109','UTS2114/UTC2114']
+            'Individual reflection' : ['UTC1102B','UTS2116'],
+            'Field trip': ['UTC1102C','UTS2103']
         }
       },
     ],
